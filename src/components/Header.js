3e7f1d35ce88b0dd0d624bd2828d06b0164d0710
HatGuy68 <irahul.2k@gmail.com>
2021-09-06 22:38:51 +0530

    Basic Components of the page added.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () => {
+
+    return (
+        <div>
+            <h1>My Header</h1>
+            {/**
+              * Each component needs to be wrapped inside a container, i.e.
+              * Shitfing this line below breaks the code.
+              **/}
+        </div>
+    )
+}
+
+export default Header;
